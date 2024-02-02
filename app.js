0d8c8ce57a5c6812b4abf87f88475af6d0fc8a0f
unknown <aziko.coder@gmail.com>
2024-02-02 23:18:50 +0500

    edit btn added

--- app.js
+++ app.js
@@ -2,37 +2,54 @@ const title = document.querySelector('.title');
 let img = document.querySelector('.img');
 img.value = 'https://picsum.photos/350/350'
 let cards = document.querySelector('.cards');
-function createCard() {
-    const card = document.createElement('ul');
-    const titleElement = document.createElement('li'); 
-    titleElement.className = 'titleCard'
-    const editTitle = document.createElement('button')
-    const imgElement = document.createElement('li'); 
-    const delCard = document.createElement('button'); 
-
-    const imgSrc = img.value; 
-    imgElement.innerHTML = `<img src="${imgSrc}" alt="${title.value}">`; 
-
-    titleElement.textContent = title.value; 
-
-    editTitle.textContent = 'edit'
-    delCard.textContent = 'Delete'
-
-    card.className = 'card';
-    card.appendChild(titleElement); 
-    card.appendChild(editTitle); 
-    card.appendChild(imgElement); 
-    card.appendChild(delCard); 
-    cards.appendChild(card);
-
-    delCard.addEventListener('click', () =>{
-        card.className = 'delClass'
-    })
-
-    // editTitle.addEventListener( 'click', () => {
-    //     const editInput = document.createElement('input')
-
-    //     card.appendChild(editInput)
-        
-    // })
+if (title.value.length == 0) {
+    function createCard() {
+        const card = document.createElement('ul');
+        const titleElement = document.createElement('li');
+        titleElement.className = 'titleCard'
+        const editTitle = document.createElement('button')
+        const imgElement = document.createElement('li');
+        const delCard = document.createElement('button');
+    
+        const imgSrc = img.value;
+        imgElement.innerHTML = `<img src="${imgSrc}" alt="${title.value}">`;
+    
+        let titleEl = titleElement.textContent = title.value;
+    
+        editTitle.textContent = 'Edit'
+        delCard.textContent = 'Delete'
+    
+        card.className = 'card';
+        card.appendChild(titleElement);
+        card.appendChild(editTitle);
+        card.appendChild(imgElement);
+        card.appendChild(delCard);
+        cards.appendChild(card);
+    
+        delCard.addEventListener('click', () => {
+            card.className = 'delClass'
+        })
+    
+    
+        editTitle.addEventListener('click', () => {
+            editTitle.classList.add('displayNone')
+            const newInput = document.createElement('input')
+            newInput.required = true;
+            newInput.value = titleEl
+            const saveBtn = document.createElement('button')
+            saveBtn.textContent = 'Save'
+            titleElement.parentNode.insertBefore(newInput, titleElement)
+            titleElement.parentNode.insertBefore(saveBtn, titleElement)
+            // titleElement.remove('')
+            titleElement.classList.add('displayNone')
+            saveBtn.addEventListener('click', () =>{
+                editTitle.classList.add('displayBlock')
+                titleElement.textContent = newInput.value
+                titleElement.classList.remove('displayNone')
+                titleElement.classList.add('displayBlock')
+                newInput.remove('')
+                saveBtn.remove('')
+            })
+        })
+    }
 }
